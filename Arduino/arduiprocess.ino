0e9baa42e77e871fe16f95cb613f570561dfbc10
willpowerstudios <creation@willpowerstudios.com>
2014-02-18 18:32:38 +0100

    retry

--- Arduino/arduiprocess.ino
+++ Arduino/arduiprocess.ino
@@ -1,22 +1,12 @@
-/* Ping))) Sensor
-  
-   This sketch reads a PING))) ultrasonic rangefinder and returns the
-   distance to the closest object in range. To do this, it sends a pulse
-   to the sensor to initiate a reading, then listens for a pulse 
-   to return.  The length of the returning pulse is proportional to 
-   the distance of the object from the sensor.
-     
-   The circuit:
-    * +V connection of the PING))) attached to +5V
-    * GND connection of the PING))) attached to ground
-    * SIG connection of the PING))) attached to digital pin 7
-
-   http://www.arduino.cc/en/Tutorial/Ping
+/* ArduiProcess -- PROCESSING
    
    created 3 Nov 2008
    by David A. Mellis
    modified 30 Aug 2011
-   by Tom Igoe
+   by WillPower
+
+   Basic UltraSound - Arduino 
+   The farther my hand it the bigger the circle
  
    This example code is in the public domain.
 
